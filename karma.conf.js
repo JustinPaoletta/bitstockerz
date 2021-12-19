2ff7208f4c23db971377e82bca8a06570f781579
JustinPaoletta <justinpaoletta@gmail.com>
2021-12-19 11:36:25 -0500

    updated app name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bitstocker'),
+      dir: require('path').join(__dirname, './coverage/bitstockerz'),
       subdir: '.',
       reporters: [
         { type: 'html' },
