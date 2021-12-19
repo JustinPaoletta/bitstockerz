2ddc7c6de570c8060666852ac1477caab4c334bc
JustinPaoletta <justinpaoletta@gmail.com>
2021-12-19 11:12:50 -0500

    move from gitlab

---
+++ crypto-server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+require('dotenv').config();
+const axios = require('axios');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const HOST = 'localhost';
+const PORT = '5555';
+const KEY = process.env.API_KEY;
+
+const server = express();
+
+server.use(cors());
+server.use(bodyParser.json());
+server.use(express.json());
+server.use(express.urlencoded());
+
+server.get('/api/news', (req, res) => {
+
+    axios.get(`https://cryptopanic.com/api/v1/posts/?auth_token=${KEY}&public=true`)
+    .then((responseObject) => {
+        res.status(200).send(responseObject.data.results);
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+});
+
+server.get('/api/trade', (req, res) => {
+
+    axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
+    .then((responseObject) => {
+        res.status(200).send(responseObject.data);
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+});
+
+server.listen(PORT, (err) => {
+    if (err) {
+        console.log('There was a problem with the server: ', err);
+    } else {
+        console.log('Crypto Server says hi from ', 'http://'+HOST+':'+PORT);
+    }
+});
---
+++ holdings-server.js
@@ -0,0 +1,73 @@
+const cors = require('cors');
+const express = require('express');
+let server = express();
+
+const host = 'localhost';
+const port = 7000;
+
+server.use('*', cors());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+const sql = require('mysql');
+
+const connection = sql.createConnection({
+    host     : 'myholdings.ce32gg8cgwze.us-east-2.rds.amazonaws.com',
+    user     : 'admin',
+    password : 'bf4oVKcALvALJeQc8m2z',
+    port     : '3306',
+    database : 'myholdings'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+    console.log('Connected to database.');
+});  
+
+
+server.get('/api/stash', function (req, res) {
+    connection.query(`SELECT * from cryptoholdings`, (error, data) => {
+        if (error) {
+            console.error();
+        } else {
+            res.send(data);
+        }
+    })
+});
+
+server.delete('/api/deleteCoin', function (req, res) {
+    console.log(req);
+    connection.query(`DELETE FROM cryptoholdings WHERE coin = '${req.body.coin}';`, (error, data) => {
+        if (error) {
+            console.error();
+        } else {
+            res.send(data);
+        }
+    })
+});
+
+server.post('/api/addCoin', function (req, res) {
+    connection.query(`INSERT INTO cryptoholdings (coin, amount)
+    VALUES ('${req.body.coin}', '${req.body.amount}');`, (error, data) => {
+        if (error) {
+            console.error();
+        } else {
+            res.send(data);
+        }
+    })
+});
+
+server.put('/api/updateCoin', function (req, res) {
+    connection.query(`UPDATE cryptoholdings SET amount = '${req.body.amount}' WHERE coin = '${req.body.coin}';`)
+});
+
+server.listen(port, (error) => {
+    if (error) {
+        console.console.error();
+    } else {
+        console.log(`Server says hi from http://${host}:${port}`);
+    }
+});
